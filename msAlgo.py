2316f9a3167b09063f1335a68ca5a112dd607f8f
reidarkl <reidarkl@stud.ntnu.no>
2015-11-04 19:58:50 +0100

    added file

---
+++ msAlgo.py
@@ -0,0 +1,20 @@
+from sys import stdin, maxint
+
+
+def korteste_rute(rekkefolge, nabomatrise, byer):
+    # SKRIV DIN KODE HER
+    pass
+
+testcases = int(stdin.readline())
+for test in range(testcases):
+    byer = int(stdin.readline())
+    rekkefolge = [int(by) for by in stdin.readline().split()]
+    nabomatrise = []
+    for by in range(byer):
+    # SKRIV DIN KODE HER
+        pass
+    print korteste_rute(rekkefolge, nabomatrise, byer)
+
+print testcases
+print byer
+print rekkefolge
